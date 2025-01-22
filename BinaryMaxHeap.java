ecef7b2088e01cb42f11508febbd66e434001237
Landon Davidson <accounts@landond.tech>
2025-01-21 21:03:19 -0800

    Add: function declarations

--- BinaryMaxHeap.java
+++ BinaryMaxHeap.java
@@ -4,5 +4,44 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-public class BinaryMaxHeap <T extends Comparable<T>> implements MyPriorityQueue<T> {    
+public class BinaryMaxHeap <T extends Comparable<T>> implements MyPriorityQueue<T> {
+    @Override
+    public void insert(T item) {
+
+    }
+
+    @Override
+    public T extract() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public void updatePriority(T item) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public void remove(T item) {
+
+    }
 }
